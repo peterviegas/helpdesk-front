d0f6cf347639e7625cbbbe297cda2ff5262c2947
Peter Viegas <peterviegas@outlook.com>
2022-07-22 12:32:30 +0100

    HTML and CSS from Home component

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Tecnico } from './../../../models/tecnico';
+
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Peter Viegas',
+      cpf: '123.456.789-10',
+      email: 'peterviegas@outlook.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '15/08/2022'
+    }
+  ]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
